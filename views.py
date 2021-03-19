5da2d45f28948eacb681e9cc6a7c12fd3cf9e2f1
Gabriel Teixeira <louistx@live.com>
2021-03-19 10:50:26 -0300

    19/03

--- views.py
+++ views.py
@@ -4,29 +4,44 @@ from app import app, login_manager, db
 from models import *
 import requests, json
 
-login_manager.login_view = 'auth'
+login_manager.login_view = 'index'
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    usr = 'coxagazzo'
-    passw = 'nosalt'
+    if request.method == 'POST':
+        formUser = request.form['username']
 
-    user = User.query.filter_by(username='coxagazzo').first()
-    login_user(user)
-    return 'Você está logado!' + current_user.username
+        user = User.query.filter_by(username=formUser).first()
+        login_user(user)
+        
+        session['username']= current_user.username
+
+        return redirect(url_for('home'))
+
+        # user = User.query.filter_by(username='coxagazzo').first()
+        # login_user(user)
+        # return 'Você está logado!' + current_user.username
 
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return 'Você foi deslogado'
+    return redirect(url_for('index'))
 
 @app.route('/')
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     return render_template('index.html')
+
+    
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html')
