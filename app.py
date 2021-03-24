4363f379bd262b5172f5ee2551121f7a894a5773
Gabriel Teixeira <louistx@live.com>
2021-03-24 17:24:34 -0300

    update

--- app.py
+++ app.py
@@ -17,9 +17,6 @@ db.create_all()
 
 temp = User(username = 'coxagazzo', password='nosalt', email='a@b.com')
 db.session.add(temp)
-post = Post(user_id = 1, text = 'TESTANDOAAAA')
-db.session.add(post)
-db.session.commit()
 
 if __name__ == "__main__":
     app.run()
